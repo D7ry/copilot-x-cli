a323bee2044877a379a0f923eaa3104dbb3ca757
d7ryLinux <yty108102@gmail.com>
2024-03-30 01:44:09 -0700

    clean up

--- src/llm.rs
+++ src/llm.rs
@@ -24,8 +24,7 @@ pub trait LLM {
 }
 
 pub struct CopilotChat {
-    jwt_token: Option<String>, //TODO: probably don't need this anymore
-    request_header: HeaderMap,
+    api_request_header: HeaderMap,
     state: Value, // a json value, conains all past conversation
 }
 
@@ -62,7 +61,7 @@ impl LLM for CopilotChat {
                 if ai_output.is_none() {
                     return "Empty response".to_string();
                 }
-                let ai_output : String = ai_output.unwrap().to_string();
+                let ai_output: String = ai_output.unwrap().to_string();
                 self.append_message(LLMRole::Assistant, &ai_output);
                 return ai_output;
             }
@@ -75,34 +74,45 @@ impl LLM for CopilotChat {
 }
 
 impl CopilotChat {
-    fn get_oauth_token() -> String {
+    fn get_oauth_token() -> Option<String> {
         match std::env::var("COPILOT_TOKEN") {
-            Ok(token) => return token,
+            Ok(token) => return Some(token),
             Err(_) => {}
         }
 
         let mut path = PathBuf::from(env::var("HOME").unwrap());
         path.push(".config/github-copilot/hosts.json");
 
-        let data = fs::read_to_string(path).expect("Unable to read file");
-        let json: Value = serde_json::from_str(&data).expect("JSON was not well-formatted");
-        let oauth_token = json["github.com"]["oauth_token"]
-            .as_str()
-            .expect("oauth_token not found");
+        match fs::read_to_string(&path) {
+            Ok(data) => {
+                let json: Value = serde_json::from_str(&data).expect("JSON was not well-formatted");
+                let oauth_token = json["github.com"]["oauth_token"]
+                    .as_str()
+                    .expect("oauth_token not found");
 
-        println!("oauth_token: {}", oauth_token);
-        return oauth_token.to_string();
+                println!("oauth_token: {}", oauth_token);
+                return Some(oauth_token.to_string());
+            }
+            Err(_) => {
+                println!("Error: Could not find the hosts.json file");
+                return None;
+            }
+        }
     }
 
     fn update_jwt_token(&mut self) {
         let rt = Runtime::new().unwrap();
         let jwt = rt.block_on(Self::get_jwt_token()).unwrap();
 
+        if jwt == "" {
+            println!("Error: Could not get jwt token");
+            return;
+        }
+
         // Update the request header with the new jwt token
         let bearer_token: String = format!("Bearer {jwt_token}", jwt_token = jwt.to_string());
 
-        println!("bearer_token: {}", bearer_token);
-        self.request_header.insert(
+        self.api_request_header.insert(
             "authorization",
             HeaderValue::from_str(&bearer_token).unwrap(),
         );
@@ -118,7 +128,7 @@ impl CopilotChat {
         let client = Client::new();
 
         // println!("state: {:?}", self.state);
-        let headers = self.request_header.clone();
+        let headers = self.api_request_header.clone();
         // println!("headers: {:?}", headers);
         let response = client
             .post("https://api.githubcopilot.com/chat/completions")
@@ -182,19 +192,20 @@ impl CopilotChat {
             let chunk = item?;
             // look for new line character, if found, print the buffer
             let chunk_str = std::str::from_utf8(&chunk).unwrap();
-            // println!("chunk");
             buf.push_str(chunk_str);
             if chunk_str.contains("\n") {
-                // println!("chunk has newline");
                 // look for all the new lines
                 let lines = buf.split("\n").collect::<Vec<&str>>();
-                let mut iter_range: usize = 0;
-
-                let last_line_is_copmlete = lines.last().unwrap().contains("\n");
-                if last_line_is_copmlete {
-                    iter_range = lines.len();
-                } else {
-                    iter_range = lines.len() - 1;
+                let iter_range: usize;
+                let last_line_is_copmlete = lines.last().unwrap().contains("\n"); // unwrap is safe, there is at least one line
+                match last_line_is_copmlete {
+                    true => {
+                        iter_range = lines.len(); // process all lines
+                    }
+                    false => {
+                        iter_range = lines.len() - 1; // don't process the last line yet, byte
+                                                      // streams are incoming
+                    }
                 }
                 for i in 0..iter_range {
                     let partial_ai_response = read_line(lines[i]);
@@ -218,46 +229,42 @@ impl CopilotChat {
     async fn get_jwt_token() -> Option<String> {
         let client = reqwest::Client::new();
 
-        let token_header = format!(
-            "token {copilot_token}",
-            copilot_token = Self::get_oauth_token()
-        );
+        let token_header: String;
 
-        let token_header_encoded = HeaderValue::from_str(&token_header).unwrap();
-
-        let header_pairs = vec![
-            ("authorization", token_header_encoded),
-            (
-                "editor-version",
-                HeaderValue::from_static("vscode/1.79.0-insider"),
-            ),
-            (
-                "editor-plugin-version",
-                HeaderValue::from_static("copilot/1.86.112"),
-            ),
-            (
-                "user-agent",
-                HeaderValue::from_static("GithubCopilot/1.86.112"),
-            ),
-            ("accept", HeaderValue::from_static("*/*")),
-        ];
-
-        let mut headers = HeaderMap::new();
-
-        for (key, value) in header_pairs {
-            headers.insert(key, value);
+        match Self::get_oauth_token() {
+            Some(token) => {
+                token_header = format!("token {copilot_token}", copilot_token = token);
+            }
+            None => {
+                return None;
+            }
         }
+
+        let mut jwt_headers: HeaderMap = [
+            ("editor-version", "vscode/1.79.0-insider"),
+            ("editor-plugin-version", "copilot/1.86.112"),
+            ("user-agent", "GithubCopilot/1.86.112"),
+            ("accept", "*/*"),
+        ]
+        .iter()
+        .map(|(k, v)| (k.parse().unwrap(), HeaderValue::from_static(*v)))
+        .collect();
+
+        jwt_headers.insert(
+            "authorization",
+            HeaderValue::from_str(&token_header).unwrap(),
+        );
 
         let res = client
             .get("https://api.github.com/copilot_internal/v2/token")
-            .headers(headers)
+            .headers(jwt_headers)
             .send()
             .await
             .unwrap();
 
         let json: Value = res.text().await.unwrap().parse().unwrap();
 
-        if (json["token"].is_null()) {
+        if json["token"].is_null() {
             return None;
         }
 
@@ -266,47 +273,30 @@ impl CopilotChat {
         return Some(token);
     }
     pub fn new() -> CopilotChat {
-        let mut map = HeaderMap::new();
-        map.insert(
-            "x-request-id",
-            HeaderValue::from_static("9d4f79c9-7104-4e24-a3ac-73349f95af63"),
-        );
-        map.insert(
-            "openai-organization",
-            HeaderValue::from_static("github-copilot"),
-        );
-        map.insert(
-            "vscode-sessionid",
-            HeaderValue::from_static("9188b680-9c71-402e-9e9d-f6d3a99f71f91684844091941"),
-        );
-        map.insert(
-            "vscode-machineid",
-            HeaderValue::from_static(
+        let map: HeaderMap = [
+            ("x-request-id", "9d4f79c9-7104-4e24-a3ac-73349f95af63"),
+            ("openai-organization", "github-copilot"),
+            (
+                "vscode-sessionid",
+                "9188b680-9c71-402e-9e9d-f6d3a99f71f91684844091941",
+            ),
+            (
+                "vscode-machineid",
                 "859856161997d243b5f349338d1bd485b6d2664faa24bed9c1f09bdff6dddb08",
             ),
-        );
-        map.insert(
-            "editor-version",
-            HeaderValue::from_static("vscode/1.79.0-insider"),
-        );
-        map.insert(
-            "editor-plugin-version",
-            HeaderValue::from_static("copilot/0.1.2023052205"),
-        );
-        map.insert(
-            "openai-intent",
-            HeaderValue::from_static("conversation-panel"),
-        );
-        map.insert("content-type", HeaderValue::from_static("application/json"));
-        map.insert(
-            "user-agent",
-            HeaderValue::from_static("GithubCopilot/0.1.2023052205"),
-        );
-        map.insert("accept", HeaderValue::from_static("*/*"));
+            ("editor-version", "vscode/1.79.0-insider"),
+            ("editor-plugin-version", "copilot/0.1.2023052205"),
+            ("openai-intent", "conversation-panel"),
+            ("content-type", "application/json"),
+            ("user-agent", "GithubCopilot/0.1.2023052205"),
+            ("accept", "*/*"),
+        ]
+        .iter()
+        .map(|(k, v)| (k.parse().unwrap(), HeaderValue::from_static(*v)))
+        .collect();
 
         let mut ret = CopilotChat {
-            jwt_token: None,
-            request_header: map,
+            api_request_header: map,
             state: serde_json::json!({
                 "intent": true,
                 "messages": [],
